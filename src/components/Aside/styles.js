d9d7dc8500a0562ed8add13aab49da1f0dbffb48
paulophdev <83602060+paulophdev@users.noreply.github.com>
2023-02-11 17:00:07 -0300

    Adicionado altura para menu no mobile

--- src/components/Aside/styles.js
+++ src/components/Aside/styles.js
@@ -4,15 +4,16 @@ export const Container = styled.div`
   background: ${(props) => props.theme.colors.backgroundPrimary};
   padding: 20px 0;
   height: 100vh;
-  position: sticky;
-  top: 0;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  position: sticky;
+  top: 0;
 
   @media (max-width: 900px) {
     flex-direction: row;
     padding: 20px;
+    height: auto;
   }
 `;
